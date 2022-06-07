9a5d842ae5395ab049fb1f1d78d6ff669e798000
Giorgia Villa <75792199+VillaGio@users.noreply.github.com>
2022-06-07 18:05:19 +0200

    upload python scripts update check

---
+++ python/auto_run_update.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+PowerShell.exe -Command "C:\Users\giorg\OneDrive\Desktop\Final Thesis\ARPA_sensors\python\main1.py"
---
+++ python/main1.py
@@ -0,0 +1,14 @@
+from pickle_functions import *
+from integrity_functions import *
+from postgres_functions import *
+from update_functions import *
+
+
+
+def main():
+    updateCheck("2022-05-29T12:00:00")
+    
+
+
+if __name__ == "__main__":
+    main()
