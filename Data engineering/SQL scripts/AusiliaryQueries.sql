8831cdd0a580f7dce2b43f1fd224147fcdc258d0
VillaGio <75792199+VillaGio@users.noreply.github.com>
2022-08-07 23:33:31 +0200

    Jobs corrections

--- Data engineering/SQL scripts/AusiliaryQueries.sql
+++ Data engineering/SQL scripts/AusiliaryQueries.sql
@@ -1,41 +1,73 @@
---Populate stations_check_datsastop (do if from Talend)
+--Populate stations_check_datsastop
 insert into stations_check_datastop
-select idsensore, storico 
-from stations 
+select idsensore
+from stations_sens 
 where storico='S' and datastop='9999-12-31 00:00:00.000'::timestamp;
 
 
 
----Populate stations_check_datsastart (do if from Talend)
+---Populate stations_check_datsastart
 insert into stations_check_datastart
-select idsensore, storico 
-from stations 
-where datastart='9999-12-31 00:00:00.000'::timestamp;
+select idsensore
+from stations_sens 
+where datastart='0001-01-01 00:00:00.000'::timestamp;
 
 
 
----Check for duplicates
+---Check for duplicates in sens_data tables
 with tabella_duplicati as(
-select  idsensore,dataora, count(*) from sens_data_2005
+select  idsensore,dataora, count(*) from sens_data_2012
 group by 1,2
 having count(*)  > 1)
 
-select  * from sens_data_2005
+select * from sens_data_2012
 where (idsensore,dataora) in(
 select idsensore,dataora from tabella_duplicati)
 
 
 
+---Check for duplicates in weather_data tables
+with tabella_duplicati as(
+select  idsensore,dataora,idoperatore,  count(*) from weather_data_2012
+group by 1,2,3
+having count(*)  > 1)
+
+select * from weather_data_2012
+where (idsensore,dataora, idoperatore) in(
+select idsensore,dataora,idoperatore from tabella_duplicati)
+
+
+
+--Rettify duplicates with NA vs VA values in 2017
+with tabella_duplicati as(
+select  idsensore,dataora, count(*) from sens_data_2017
+group by 1,2
+having count(*)  > 1)
+
+delete from sens_data_2017
+where (idsensore,dataora) in(
+select idsensore,dataora from tabella_duplicati) and stato = 'NA'
+
+
+
+--Rettify duplicate with non-updated idOperatore in 2013
+delete from sens_data_2013 
+where dataora = '2013-11-20 07:00:00.000'::timestamp and idoperatore =15504;
+
+
+
 ---Check for daylight changing time (set as 01:59:00)
-select * from sens_data_2005 
+select * from sens_data_2021 
 where extract (minute from dataora) != 00;
 
 
 
 ---CATALOG queries to rettify table station  
+
+---Rettify datastop
 SELECT tablename 
 FROM pg_catalog.pg_tables 
-where schemaname = 'woneli' and tablename like 'sens_data_%';
+where schemaname = 'public' and tablename like 'sens_data_%';
 
 
 select idsensore, max(dataora) 
@@ -47,6 +79,25 @@ where sens_data_2021.idsensore = stations_check_datastop.idsensore)
 group by idsensore;
 
 
-select idsensore, max(ultima_acquisizione) from prova 
+select idsensore, max(ultima_acquisizione) from stations_check_datastop 
 group by idsensore;
 
+
+
+---Rettify datastart
+SELECT tablename FROM pg_catalog.pg_tables
+where schemaname = 'public' and tablename like 'sens_data_%'
+order by tablename desc;
+
+
+select idsensore, min(dataora) 
+from sens_data_2021
+where exists (
+select * 
+from stations_check_datastop 
+where sens_data_2021.idsensore = stations_check_datastop.idsensore) 
+group by idsensore;
+
+
+select idsensore, min(prima_acquisizione) from stations_check_datastart
+group by idsensore;
--- Data engineering/SQL scripts/CreateTables.sql
+++ Data engineering/SQL scripts/CreateTables.sql
@@ -10,8 +10,8 @@ create table logs(
 
 
 
----Create table stations for stations history 
-CREATE TABLE stations(
+---Create table stations_sens for sensors stations history 
+CREATE TABLE stations_sens(
 	idsensore int8 NULL,
 	nometiposensore varchar NULL,
 	unitamisura varchar NULL,
@@ -25,6 +25,27 @@ CREATE TABLE stations(
 	datastop timestamp NULL,
 	utm_nord int8 NULL,
 	utm_est int8 NULL,
+	lat float8 NULL,
+	lng float8 null,
+	coordinate varchar null,
+	primary key (idsensore, datastart)
+);
+
+
+---Create table stations_weather for weather stations history 
+CREATE TABLE stations_weather(
+	idsensore int8 NULL,
+	tipologia varchar NULL,
+	unit_dimisura varchar NULL,
+	idstazione int8 NULL,
+	nomestazione varchar null,
+	quota int8 NULL,
+	provincia varchar NULL,
+	storico varchar(2) NULL,
+	datastart timestamp,
+	datastop timestamp NULL,
+	cgb_nord int8 NULL,
+	cgb_est int8 NULL,
 	lat float8 NULL,
 	lng float8 null,
 	coordinate varchar null,
@@ -49,8 +70,19 @@ prima_acquisizione timestamp null
 
 
 
+---Create table for sensors data
+create table sens_data_2022 (
+	idsensore int8 NULL,
+	dataora timestamp NULL,
+	valore float8 NULL,
+	stato varchar(2) NULL,
+	idoperatore int4 NULL
+);
+
+
+
 ---Create table for weather data
-create table sens_weather_2021 (
+create table weather_data_2022 (
 	idsensore int8 NULL,
 	dataora timestamp NULL,
 	valore float8 NULL,
